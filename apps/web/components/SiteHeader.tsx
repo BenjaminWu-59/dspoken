744be1ce6e96f74ef88f8fdf4e965c381b252a62
benjamin <wuqiong3699@gmail.com>
2024-09-19 19:56:30 +0800

    首页topnav修正

--- apps/web/components/SiteHeader.tsx
+++ apps/web/components/SiteHeader.tsx
@@ -5,17 +5,24 @@ import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import Link from 'next/link';
 import { signout } from "@/api/auth";
 import { getUser, User } from '@/api/user';
 import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
+
 
 
 
 const SiteHeader = () => {
+  const router = useRouter();
+  
   const [user, setUser] = useState<User | null>(null);
   useEffect(() => {
     const fetchUser = async () => {
@@ -34,31 +41,32 @@ const SiteHeader = () => {
         <div className="flex justify-end items-center">
           {user ? (
             <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <div className="flex justify-center items-center">
-                  <Image
-                    src="/avatar.svg"
-                    alt="avatar"
-                    className="rounded-full"
-                    width={50}
-                    height={50}
-                    priority
-                  />
-                </div>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="p-2 flex flex-col justify-center items-center">
-                <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
-                <div className="w-full flex flex-col">
-                  <hr className="border-t-1 border-gray-300 py-3" />
-                  <Button className="custom-button"
-                    onClick={() => signout()}
-                  >
-                    退出
-                  </Button>
-                </div>
-
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <div className="w-16 h-16 flex justify-center items-center">
+                <img
+                  src={user?.avatar}
+                  alt="avatar"
+                  className="w-full h-full rounded-full object-cover"
+                />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-48 mr-3">
+              <DropdownMenuLabel>
+                 <p className="text-base">{user?.name}</p>
+                 <p className="text-gray-500">{user?.email}</p>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem onClick={() => router.push('/dashboard/account')}>
+                  Profile
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => signout()}>
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
           ) : (
             <Link href="/login" passHref>
               <Button variant="outline">登陆</Button>
